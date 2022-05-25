0baf13984ad04880bc6e5fd3407467d957ab579b
truzmeto <talantruzmetov@gmail.com>
2022-05-24 22:48:28 -0700

    updated fig

--- BKit/__init__.py
+++ BKit/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8cd3bcffd54c58d4117324b9945d071bd760902cc06cb4f95f751c218cc48e0b
-size 414
+oid sha256:400ebc5d823d92f9efba03a1a7416867002457a2648c1282fa9558826bf38b77
+size 375
