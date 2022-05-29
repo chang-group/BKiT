74144380a8292a084dd40136e1219de79d5ecac3
truzmeto <talantruzmetov@gmail.com>
2022-05-29 12:55:06 -0700

    adding all 3 notebooks

--- BKit/__init__.py
+++ BKit/__init__.py
@@ -7,3 +7,5 @@ from .InterpolateCurve import InterpolatePath
 from .ConstructMilestones3D import rotation_matrix
 from .ConstructMilestones3D import ConstructMilestones3D
 from .ConstructMilestones3D import SortFrames
+from .TransitionKernel import TransitionKernel
+from .KinSolvers import KineticsQK
