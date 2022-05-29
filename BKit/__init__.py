16d9b819410c032910aa8977c14459f9252fb39f
truzmeto <talantruzmetov@gmail.com>
2022-05-29 12:55:06 -0700

    adding all 3 notebooks

--- BKit/__init__.py
+++ BKit/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:400ebc5d823d92f9efba03a1a7416867002457a2648c1282fa9558826bf38b77
-size 375
+oid sha256:83fbfa61ac777b3ff93275e68268c53307aa134ca7924f0e931911f503cd944d
+size 457
---
+++ notebooks/nb2/nb2.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c07534104b4e9694298ffdd860bca752467e143f211901e48f719878544f745e
+size 306552
