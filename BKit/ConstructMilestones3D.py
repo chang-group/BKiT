113ae0e97c24c39314210499df060093f46c74ad
truzmeto <talantruzmetov@gmail.com>
2022-10-19 21:44:45 -0700

    variable disk size is implemented

--- BKit/ConstructMilestones3D.py
+++ BKit/ConstructMilestones3D.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:adb0fb238ee367bc3268355eb23726b628a19637a0aacef6eb7dabc7a1b3b000
-size 14112
+oid sha256:27234124e503ca36dd49b5394a10b74630bbe19e57e8cc54214c2566b902c288
+size 14117
