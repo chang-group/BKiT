0a7a94a508d49ec4bfcc4eea76d67700c0968027
truzmeto <talantruzmetov@gmail.com>
2024-01-22 23:17:40 -0800

    Discard traj if initial frame is outside the path

--- BKit/TransitionKernel.py
+++ BKit/TransitionKernel.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e22e560f854ab836f8badc353b183b950e87126836cb1405842e32fd6fccf266
-size 7709
+oid sha256:af37899b415497f0d839cfd72a7e87e56d7acee1ba9003e48fe9f288c469b8f7
+size 7630
