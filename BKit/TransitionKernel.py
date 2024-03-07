70c784d6f2c715450b00e7c5ed76c3f649e01b66
truzmeto <talantruzmetov@gmail.com>
2024-03-06 22:50:43 -0800

    fixed strict method

--- BKit/TransitionKernel.py
+++ BKit/TransitionKernel.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c509c7515998ee0df8ec662d9906177cbade01fe056f48e62eed8aea531a03af
-size 7745
+oid sha256:a4934db4ac54d63482376b34dc9ce2f4f0a8058e2ff4347921a5a24659495131
+size 7863
